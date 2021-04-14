31661d80bb2e3dc4cc1310da49b43dca83246690
roy yoo <royyoo123@gmail.com>
2021-04-13 20:28:37 -0700

    added user form component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './Users/AddUser';
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <AddUser  />
+    </div>
+  );
+}
+
+export default App;
