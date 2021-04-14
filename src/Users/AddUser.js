ca5555fda43922ed2cefc4845d66899f76eb5c33
roy yoo <royyoo123@gmail.com>
2021-04-14 13:47:10 -0700

    button and card component added

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React from 'react';
-
+import Card from './AddCard';
+import classes from './AddUser.module.css';
+import Button from './AddButton';
 const AddUser = (props) => {
 	const addUserHandler = (event) => {
 		event.preventDefault();
 	};
 
 	return(
-		<form>
-			<label htmlFor="username">Username</label>
-			<input id="username" type="text" />
-			<label htmlFor="age">Age (Years)</label>
-			<input id="age" type="number" />
-			<button type="submit">Add User</button>
-		</form>
+		<Card className={classes.input}>
+			<form>
+				<label htmlFor="username">Username</label>
+				<input id="username" type="text" />
+				<label htmlFor="age">Age (Years)</label>
+				<input id="age" type="number" />
+				<Button type="submit">Add User</Button>
+			</form>
+		</Card>
 	);
 };
 
